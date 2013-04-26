118ffe3d3eb8663d86ed7e37a0896ea8f5d46178
Samuel Jun <samuel.h.jun@gmail.com>
2013-04-26 01:43:03 -0700

    Made Changes based on the 'Database Scribbles' doc
    I added a few things based on Ryan's 'Database Scribbles' google doc. I
    mainly added a few parameters that I was missing. I also added a Pins
    table between the Nodes and Data layer because I didn't realize that
    each Node can have multiple pins.

--- create.sql
+++ create.sql
@@ -1,23 +1,32 @@
 CREATE TABLE Users (
-    user_id INT PRIMARY KEY,
+    user_id INT PRIMARY KEY, --Might just be Facebook provided ID
+    email VARCHAR(50) NOT NULL,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    email VARCHAR(50)
+    api_key VARCHAR(50),
+    pin INT
 );
 CREATE TABLE Hubs (
     hub_id VARCHAR(30) PRIMARY KEY,
     user_id INT NOT NULL,
-    name VARCHAR(50)
+    pan_id INT, --XBee
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50)
 );
 CREATE TABLE Nodes (
-    node_id VARCHAR(30) PRIMARY KEY,
-    hub_id VARCHAR(30) NOT NULL,
-    name VARCHAR(50)
+    node_id INT PRIMARY KEY,
+    hub_id INT NOT NULL,
+    address VARCHAR(16), --hex
+    name VARCHAR(50) NOT NULL,
+    type VARCHAR(10) NOT NULL
+);
+CREATE TABLE Pins (
+    pin_id INT PRIMARY KEY,
+    node_id INT NOT NULL,
+    data_type VARCHAR(10) NOT NULL
 );
 CREATE TABLE Node_Data (
-    node_id VARCHAR(30) NOT NULL,
-    data_type VARCHAR(10) NOT NULL,
-    data_point VARCHAR(50) NOT NULL,
-    time TIMESTAMP NOT NULL,
+    pin_id INT NOT NULL,
+    pin_value VARCHAR(50) NOT NULL,
+    time TIMESTAMP NOT NULL
 );
-
